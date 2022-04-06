5be5b07d75a0658864b6e660f90075e82b950f9a
Anton Kamnev <34867914+anteridy@users.noreply.github.com>
2022-04-06 13:03:36 +0200

    Delete ForGitHub directory

--- ForGitHub/Segmentation_Cluster_SubmitJobs.sh
+++
@@ -1,7 +0,0 @@
-#.sh file to call iteratively cluster CP jobs:
-
-batchFileName=Cluster_RunSingleJob.sh
-for i in {1..312}; do
-  sbatch --export=iter=$i --output /scratch//lab_boztug/akamnev/Plate/JobLogs/iter$i"_%j.log" --job-name=$i"_CPProfilingAPID" --error /scratch//lab_boztug/akamnev/Plate/JobLogs/iter$i"_%j.err" $batchFileName;
-  sleep 3;
-done
